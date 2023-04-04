b70de6274f5d744cd3a9f305b8990f53430244d7
Alexey Dyatlov <alexey.dyatlov@yandex.ru>
2023-04-05 02:25:25 +0300

    feature: добавил классы CustomLinkedList и Node

---
+++ src/tasks/Node.java
@@ -0,0 +1,37 @@
+package tasks;
+
+public class Node<T> {
+    Node<T> prevNode;
+    Node<T> nextNode;
+    T data;
+
+    public Node(T data, Node<T> nextNode, Node<T> prevNode) {
+        this.data = data;
+        this.prevNode = prevNode;
+        this.nextNode = nextNode;
+    }
+
+    public Node<T> getPrevNode() {
+        return prevNode;
+    }
+
+    public void setPrevNode(Node<T> prevNode) {
+        this.prevNode = prevNode;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
