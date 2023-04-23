8244889fffa3142841b9a29fc236e7e9d1127be9
Alexey Dyatlov <alexey.dyatlov@yandex.ru>
2023-04-24 01:35:17 +0300

    feat: добавлен класс FileBackedTasksManager

---
+++ src/managers/FileBackedTasksManager.java
@@ -0,0 +1,13 @@
+package managers;
+
+import tasks.Epic;
+import tasks.SubTask;
+import tasks.Task;
+
+import java.util.ArrayList;
+import java.util.EventObject;
+import java.util.List;
+
+public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
+
+}
