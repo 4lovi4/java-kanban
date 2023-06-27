25167f95daf5ef49f66b67aec28bb54207d414a3
Alexey Dyatlov <alexey.dyatlov@yandex.ru>
2023-06-27 03:38:34 +0300

    feat: проверка на пустые списки задач для InMemoryTaskManager

--- test/managers/InMemoryTaskManagerTest.java
+++ test/managers/InMemoryTaskManagerTest.java
@@ -1,9 +1,19 @@
 package managers;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
 
     @Override
     InMemoryTaskManager getTaskManager() {
         return new InMemoryTaskManager();
+    }
+
+    @Test
+    public void managerShouldHaveEmptyAllTypesTasksLists() {
+        assertTrue(taskManager.getAllTasks().isEmpty() || taskManager.getAllSubTasks().isEmpty()
+                || taskManager.getAllEpics().isEmpty());
     }
 }
